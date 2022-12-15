9613960ea8015aaf476e1bef807b2221ee66d5be
Pedromellodev <pedromello19122006@gmail.com>
2022-12-15 14:29:48 -0300

    Conceito de váriaveis no Python

---
+++ Pt 1 (Váriaveis e Objetos)/variaveis-e-objetos-pt2.py
@@ -0,0 +1,22 @@
+x = int(input("Um número: "))
+y = int(input("Outro número: "))
+z = 100
+w = x + y * z / 100
+
+print(w)
+print("Esse texto será impresso no console.")
+print("Ao lado existem duas váriaveis:", x, "e", y)
+
+n = "Texto"
+print(type(n))
+print(type(w))
+
+var = input("Informe o valor ")
+var = int(var)
+print(type(var))
+var = float(var)
+print(type(var))
+
+"""
+Isso é um comentário de múltiplas linhas
+"""
---
+++ Pt 1 (Váriaveis e Objetos)/variaveis-e-objetos.py
@@ -0,0 +1,8 @@
+# Váriaveis são espaços em memória que armazenam dados
+
+x = 2 #Int
+y = 3 #Float
+soma = x + y
+
+print("A soma dos números é", soma)
+
